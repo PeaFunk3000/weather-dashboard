f22171b2ef6016d9150711157c13e8fe64972b83
PeaFunk3000 <80793315+PeaFunk3000@users.noreply.github.com>
2023-01-27 19:47:54 +0000

    fiveDayForecast base logic, response correct

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,8 +10,9 @@ console.log("Hello World!");
 // my API key 5b86dd1981e818e717862cad25215448
 const myAPI = "5b86dd1981e818e717862cad25215448"
 // --------------------------------------------------------------------------------------------
+
 var cityName = $("#search-input").val()
-var latLon = []
+
 // Event listener for search button element
 $("#search-button").on("click", function(event) {
     event.preventDefault();
@@ -23,38 +24,23 @@ $("#search-button").on("click", function(event) {
         method: "GET"
       }).then(function(response){
         console.log(response);
-        latLon.push(response[0].lat, response[0].lon)
-      });
-      console.log(latLon);
-      fiveDayForecast();
+        fiveDayForecast(response[0].lat, response[0].lon);
+      });   
 })
 
-function fiveDayForecast() {
-    var queryURL = 
-}
-
-
-
-
-
-// var cityName = "London";
-// var limit = 5;
-// var stateCode;
-// var countryCode;
-
-// var lat;
-// var lon;
-
-
-// var locationURL = "http://api.openweathermap.org/geo/1.0/direct?q=" + cityName + "&appid=" + myAPI
-// var weatherURL = "https://api.openweathermap.org/data/2.5/forecast?lat=" + lat + "&lon=" + lon + "&appid=" + myAPI
-
-
-// AJAX call
-// $.ajax({
-//     url: queryURL,
-//     method: "GET"
-//   }).then(function(response){
-//     console.log(response);
-
-//   });
+function fiveDayForecast(lat, lon) {
+    var queryURL = "https://api.openweathermap.org/data/2.5/forecast?lat=" + lat + "&lon=" + lon + "&appid=" + myAPI
+    console.log(queryURL);
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+      }).then(function(response){
+        console.log(response);
+        console.log(response.city.name);
+        var today = $("<div>");
+        today.addClass("today");
+        var todayH2 = $("<h2>");
+        todayH2.text(response.city.name);
+        today.append(todayH2);
+        $("#today").append(today);
+})}
