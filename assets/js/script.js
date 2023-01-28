11aee1c353249fddf9fd54e65cdd97defd7f8871
PeaFunk3000 <80793315+PeaFunk3000@users.noreply.github.com>
2023-01-28 15:26:59 +0000

    scrapped initial logic, new js, data handling via obj in new logic script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,50 +1,79 @@
 console.log("Hello World!");
 
 const myAPI = "5b86dd1981e818e717862cad25215448"
-var cityName = $("#search-input").val()
 
 
 // Event listener for search button element
-$("#search-button").on("click", function(event) {
+$("#search-button").on("click", function (event) {
     event.preventDefault();
     var cityName = $("#search-input").val()
+    cityName.toLowerCase();
+    cityName[0].toUpperCase();
     console.log(cityName);
-    var queryURL = "http://api.openweathermap.org/geo/1.0/direct?q=" + cityName + "&appid=" + myAPI ;
+
+    var queryURL = "http://api.openweathermap.org/geo/1.0/direct?q=" + cityName + "&appid=" + myAPI;
     $.ajax({
         url: queryURL,
         method: "GET"
-      }).then(fiveDayForecast)
-      });   
+    }).then(fiveDayForecast)
+
+});
 
 function fiveDayForecast(response) {
     console.log(response);
     var lat = response[0].lat;
     var lon = response[0].lon;
-    var today = $("<div>");
-        today.addClass("today");
-        var todayH2 = $("<h2>");
-        var historyBtn = $("<button>");
-        todayH2.text(response[0].name);
-        today.append(todayH2);
-        $("#today").append(today);
-        historyBtn.addClass("historyBtn");
-        historyBtn.text(response[0].name);
-        $("#history").prepend(historyBtn);
+    var theCity = response[0].name;
+    console.log(theCity);
 
-
-    var queryURL = "https://api.openweathermap.org/data/2.5/forecast?lat=" + lat + "&lon=" + lon + "&appid=" + myAPI ;
-    console.log(queryURL);
+    var queryURL = "https://api.openweathermap.org/data/2.5/forecast?lat=" + lat + "&lon=" + lon + "&appid=" + myAPI;
 
     $.ajax({
         url: queryURL,
         method: "GET"
-      }).then(function(response){
-        console.log(response);
-        // grab date from response
-        var dateArr = response.list[0].dt_txt.split(' ')
-        console.log(dateArr);
+    }).then(mapForecast)
+}
 
-        var theDate = moment(dateArr[0]).format("D/M/YYYY");
-        todayH2.append( "" + "(" + theDate + ")");
-        
-})}
+function mapForecast (response) {
+    console.log(response);
+    console.log(response.list[0]);
+    // present Temp (kelvin)
+    console.log(response.list[0].main.temp);
+    // present Humidity
+    console.log(response.list[0].main.humidity);
+    // present Wind speed (KPH)
+    console.log(response.list[0].wind.speed);
+    // present Weather Cond. (fa-icon)
+    console.log(response.list[0].weather[0].description);
+    var weatherToday = {
+        temp: response.list[0].main.temp,
+        humidity: response.list[0].main.humidity,
+        windSpeed: response.list[0].wind.speed,
+        weatherCond: response.list[0].weather[0].description,
+    }
+    console.log(weatherToday);
+
+    var fiveDayForecast = [];
+    
+    for (let i = 0; i < response.list.length; i = i + 8) {
+        var day = {temp: response.list[i].main.temp,
+        humidity: response.list[i].main.humidity,
+        windSpeed: response.list[i].wind.speed,
+        weatherCond: response.list[i].weather[0].description,
+        }
+        fiveDayForecast.push(day);
+    }
+    console.log(fiveDayForecast);
+}
+
+
+
+
+
+
+
+
+
+
+
+
